474d9b815cea8f27da1113e934ce1b81529f4ea8
narendrakpatel <narpatelk@gmail.com>
2019-03-18 23:00:54 +0530

    define tokens

---
+++ token.ml
@@ -0,0 +1,18 @@
+(*===----------------------------------------------------------------------====
+ * Lexer tokens
+ *===----------------------------------------------------------------------===*)
+
+(*
+ * Each token returned by the lexer includes a token code and potentially some metadata.
+ * The lexer returns these 'Kwd' if it is an unknown character, otherwise one of these
+ * others for known things. *)
+
+type token =
+  (* commands *)
+  | Def | Extern
+
+  (* primary *)
+  | Ident of string | Number of float
+
+  (* unknown *)
+  | Kwd of char
