9ae44de4997e750739fddbda755a319bef1843eb
narendrakpatel <narpatelk@gmail.com>
2019-03-19 20:41:07 +0530

    add driver code

--- toy.ml
+++ toy.ml
@@ -5,7 +5,17 @@
 let main () =
   (* Install standard binary operators
    * 1 is considered as lowest precedence *)
-   Hashtbl.add Parser.binop_precedence '<' 10;
-   Hashtbl.add Parser.binop_precedence '+' 20;
-   Hashtbl.add Parser.binop_precedence '-' 20;
-   Hashtbl.add Parser.binop_precedence '*' 40; (* highest*)
+  Hashtbl.add Parser.binop_precedence '<' 10;
+  Hashtbl.add Parser.binop_precedence '+' 20;
+  Hashtbl.add Parser.binop_precedence '-' 20;
+  Hashtbl.add Parser.binop_precedence '*' 40; (* highest*)
+
+  (* process first token *)
+  print_string "ready> "; flush stdout;
+  let stream = Lexer.lex (Stream.of_channel stdin) in
+
+  (* run the main "interpreter loop" *)
+  Toplevel.main_loop stream;
+;;
+
+main()
